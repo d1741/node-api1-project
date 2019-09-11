7d537efa517394fb1c3a4d33e45a784a7fb49ebf
d1741 <traciecodes@gmail.com>
2019-09-11 16:24:28 -0500

    Set up server on port 4000. Checked that it's working using insomnia

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // implement your API here
+
+
+const express = require('express');
+//the following pulls in the information from the dummy database
+const database = require('./data/db');
+
+const server = express();
+
+//initial get request to test the server on insomnia
+server.get('/', (req, res) => {
+    res.send("<h1>This is my first time.</h1>");
+})
+
+//told the server to listen on port 4000 for changes and gave a message for me to know whether or not it's working
+const port = 4000;
+server.listen(port, () => console.log('The API is running'));
